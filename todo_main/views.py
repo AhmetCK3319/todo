7420ca0fff57a0fecbb6675a9f17ebc0e3927f6e
Ahmet <ahmetcankoca1990@hotmail.com>
2023-09-22 23:22:04 +0300

    task_edit

--- todo_main/views.py
+++ todo_main/views.py
@@ -4,8 +4,10 @@ from todoapp.models import Task
 
 
 def home(request):
-    tasks=Task.objects.filter(is_complate=False).order_by('-created_at')
+    tasks=Task.objects.filter(is_complate=False).order_by('-updated_at')
+    complated_task=Task.objects.filter(is_complate=True).order_by('-created_at')
     context={
         'tasks':tasks,
+        'complated_task':complated_task,
     }
     return render(request,'home.html',context)
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from django.http import HttpResponse
 from .models import Task
 
@@ -9,3 +9,33 @@ def addTask(request):
     Task.objects.create(task=task) 
 
     return redirect('home')
+
+def mark_as_done(request,pk):
+    task=get_object_or_404(Task,pk=pk)
+    task.is_complate=True
+    task.save()
+    return redirect('home')
+
+def mark_as_undone(request,pk):
+    task=get_object_or_404(Task,pk=pk)
+    task.is_complate=False
+    task.save()
+    return redirect('home')
+
+def edit_task(request,pk):
+    get_task=get_object_or_404(Task,pk=pk)
+    # metod post mu diye kontrol et
+    if request.method =='POST':
+        # metod post ise imput name alanı iste
+        new_task=request.POST['task']
+         # Task nesnesinin "task" alanını güncelle
+        get_task.task=new_task
+        # Değişikliği veritabanına kaydet
+        get_task.save()
+        # dönmek istediğin konuma git
+        return redirect('home')
+    else:    
+        context={
+            'get_task':get_task,
+        }
+        return render(request,'edit_task.html',context)
